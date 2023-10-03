2de78e6bfc84a145910a4f5bde62e6f9bb3c8cd5
Alex Carter <victoryforphil@gmail.com>
2023-10-02 22:02:24 -0700

    New base project

--- README.md
+++
@@ -1,2 +0,0 @@
-# victory-terminal
-Electron based UART Terminal since every other one doesn't fit my fancy
--- renderer/pages/serial_out.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SerialOut = (props) =>{
-    return(
-        <h1>Serial Out</h1>
-    )
-}
-
-export default SerialOut;
