1f096ba3a37c5effbbaaf9ac0ac76e116a3be73a
Alex Carter <victoryforphil@gmail.com>
2023-10-15 16:24:42 -0700

    Nightly?

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -27,19 +27,14 @@ impl TerminalApp {
 
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
-        if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        }
+
         
         Default::default()
     }
 }
 
 impl eframe::App for TerminalApp {
-    /// Called by the frame work to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
-    }
+ 
 
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
